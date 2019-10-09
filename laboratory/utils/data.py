4058898ba84d959ff3370832a31f490c8be39348
Samuel Scott Jennings <a1654095@adelaide.edu.au>
2019-10-09 15:01:10 +1030

    remove old Data obj and fix txt load

--- laboratory/utils/data.py
+++ laboratory/utils/data.py
@@ -33,80 +33,30 @@ def data_dict():
                             'offset': []},
             'freq':None}
 
-class Data():
-    """Storage for all data collected during experiments. Data file are loaded into this object for processing and plotting
+def dt_to_hours(dtlist):
+    return [(t-dtlist[0]).total_seconds()/60/60 for t in dtlist]
 
-    =============== ===========================================================
-    Attributes      Description
-    =============== ===========================================================
-    freq            array of frequencies for use by the LCR meter
-    filename        name of the file being used
-    time            times for each measurement
-    thermo          stores thermopower data
-    gas             stores gas and fugacity data
-    temp            stores temperature data
-    imp             stores impedance data
-    xpos            stores stage x position at each measurement
-    =============== ===========================================================
+def process_data(data):
 
-    :example:
+    data['time_elapsed'] = dt_to_hours(data['time'])
 
-    >>> lab = Laboratory.Setup('somefile.dat')
-    >>> print(lab.data.temp.indicated)
-    [100,105,110,115,120]
-    """
-    def __init__(self,freq=None,filename=None):
+    # settings = {}
+    # for key in ['freq','file_name','time','step_time']:
+    #     settings[key] = data[key]
+    #     del data[key]
 
-        self.thermo = pd.DataFrame(columns=['indicated','target','tref','te1','te2','voltage'])
-        self.gas = Gas()
-        self.imp = pd.DataFrame(columns=['z','theta'])
-        self.filename = filename
-        self.time = []
-        self.stage_position = []
-        self.step_time = []
-        self.fugacity = pd.DataFrame(columns=['fugacity','ratio','offset'])
-        self.load_frequencies()
+    # data = pd.DataFrame(data)
+    # data['time']['actual'] = settings['time']
+    # data['time']['elapsed'] = dt_to_hours(settings['time'])
 
-    def __repr__(self):
-        pd.set_option('display.max_rows', 5)
-        """String representation of the :class:`Drivers.LCR` object."""
-        return "\nFilename:\t{}\nFrequencies:\t{} (length={})\nTime:\t\t{} (length={})\nStage position:\t{} (length={})\nStep time:\t{} (length={})\n\nThermopower:\n\n{}\n\nCO2:\n\n{}\n\nCO_a:\n\n{}\n\nCO_b:\n\n{}\n\nH2:\n\n{}\n\nImpedance:\n\n{}\n\n".format(
-            '/'.join(self.filename.split('\\')[-2:]),
-            type(self.freq), len(self.freq),
-            type(self.time), len(self.time),
-            type(self.stage_position), len(self.stage_position),
-            type(self.step_time), len(self.step_time),
-            self.thermo,
-            self.gas.co2,
-            self.gas.co_a,
-            self.gas.co_b,
-            self.gas.h2,
-            self.imp,
-            )
-            
-    def load_frequencies(self,min=config.MINIMUM_FREQ,max=config.MAXIMUM_FREQ,n=50,log=True):
-        """Creates an np.array of frequency values
-        
-            :param min: the minimum frequency value
-            :type min: float, int
-        
-            :param max: the maximum frequency value
-            :type max: float, int
-
-            :param n: the desired number of frequency values to be populated between the min and max values
-            :type min: int
-            """
-        if log:
-            self.freq = np.around(np.geomspace(min,max,n))
-        else:
-            self.freq = np.around(np.linspace(min,max,n))
+    return data
 
 
 def load_data(filename):
     if filename.endswith('.txt'):
-         return _load_text('laboratory/datafiles/' + filename)
+         return process_data(_load_text('laboratory/datafiles/' + filename))
     elif filename.endswith('.pkl'):
-         return _load_pkl('laboratory/datafiles/' + filename)
+         return process_data(_load_pkl('laboratory/datafiles/' + filename))
     else:
         raise ValueError('Unsupported filetype! Must be .txt or .pkl')
 
@@ -116,6 +66,14 @@ def _load_text(filename):
     :param filename: name of the file to be parsed
     :type filename: str
     """
+
+    def append_data(data_type,keys,vals):
+        if data_type != 'lcr':
+            vals = [float(v) for v in vals]
+            
+        for key, val in zip(keys,vals):
+            data[data_type][key].append(val)
+
     data = data_dict()
     data['filename'] = filename
 
@@ -125,9 +83,6 @@ def _load_text(filename):
         Z,theta = [],[]
         for line in datafile:
 
-            #split line into space delimited tokens
-            # line = list(filter(None, line.split(' ')))
-
             if line.startswith('frequencies'):
                 line = re.findall(r'(?<=\[).*?(?=\])', line)[0].split(',')
                 data['freq'] = np.array([float(f) for f in line])
@@ -135,41 +90,43 @@ def _load_text(filename):
 
             line = line.split()
 
-            #for time measurements...
-            if line[0] == 'D':                 #D == Datetime
-                dt_obj = dt.strptime(token[1].rstrip(),'%H:%M.%S_%d-%m-%Y')
-                data.time.append(dt_obj)
+            if not line: continue
 
-            #for gas measurements...
-            elif line[0] == 'G':                 #G == Gas
-                gas_type = line[1]
-                gas = getattr(data.gas,gas_type)
+            line_id = line[0]
+            vals = line[1:]
 
-                gas.mass_flow.append(float(line[2]))
-                gas.pressure.append(float(line[3]))
-                gas.temperature.append(float(line[4]))
-                gas.vol_flow.append(float(line[5]))
-                gas.setpoint.append(float(line[6]))
+            if line_id == 'D':       
+                if len(vals) == 2:
+                    data['time'].append(dt.strptime('_'.join(vals),'%H:%M.%S_%d-%m-%Y'))
+                else:
+                    append_data('daq',['reference','thermo_1','thermo_2','voltage'],vals)
 
-            #for furnace temperature measurements...
-            elif line[0] == 'F':       #F = Furnace
-                data.temp.target.append(float(line[1]))
-                data.temp.indicated.append(float(line[2]))
+            # if line_id == 'Time'[0]:
+            #     data['time'].append(dt.strptime('_'.join(vals),'%H:%M.%S_%d-%m-%Y'))
 
-            #for thermopower measurements...
-            elif line[0] == 'T':               #T = thermopower
-                data.thermo.tref.append(float(line[1]))
-                data.thermo.te1.append(float(line[2]))
-                data.thermo.te2.append(float(line[3]))
-                data.thermo.volt.append(float(line[4]))
+            # elif line_id == 'DAQ'[0]:
+            #     append_data('daq',['reference','thermo_1','thermo_2','voltage'],vals)
 
-            #if the data line being read relates to impedance measurements...
-            elif line[0] == 'Z':               #Z = impedance
-                Z.append(float(line[1]))
-                theta.append(float(line[2]))
-                if len(Z) == len(data.freq):
-                    data.imp.Z.append(Z)
-                    data.imp.theta.append(theta)
+            elif line_id == 'Step Time'[0]:
+                data['step_time'].append(dt.strptime('_'.join(vals),'%H:%M.%S_%d-%m-%Y'))
+
+            elif line_id == 'Gas'[0]:
+                pass
+
+            elif line_id == 'Furnace'[0]:
+                append_data('furnace',['target','indicated'],vals)
+
+            elif line_id == 'Motor'[0]:
+                append_data('motor',['position'],vals)
+            
+            elif line_id == 'X':    # Fugacity
+                append_data('fugacity',['fugacity','ratio','offset'],vals)
+
+            elif line_id == 'Z':       #Impedance
+                Z.append(float(vals[0]))
+                theta.append(float(vals[1]))
+                if len(Z) == len(data['freq']):
+                    append_data('lcr',['z','theta'],[Z,theta])
                     Z, theta = [],[]
 
     return data
@@ -185,10 +142,6 @@ def _load_pkl(filename):
 
     with open(filename, 'rb') as f:  # Overwrites any existing file.
         return pickle.load(f)
-
-def append_data(filename,data):
-
-    pass
 
 def save_obj(obj, filename):
     """Saves an object instance as a .pkl file for later retrieval. Can be loaded again using :meth:'Utils.load_obj'
