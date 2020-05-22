33a3227e58924abe66af28654987bae923cd5808
SSJenny90 <samuel.scott.jennings@gmail.com>
2020-05-22 10:47:39 +0930

    added csv attachment to email notifications

--- laboratory/utils/notifications.py
+++ laboratory/utils/notifications.py
@@ -3,8 +3,15 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 import smtplib
 from laboratory import config
+import os 
 
-def send_email(message,cc=False,logfile=False,datafile=False):
+MESSAGES = dict(
+    error = 'I\'ve got some bad news! The {} is no longer sending or receiving messages so I\'m going to shut down the lab until you can come take a look.',
+    delayed_start = 'It just ticked over to {} so I\'m going to set up the instruments and get things underway.',
+    success = "Just letting you know that step {} is now complete! I\'m going to set the temperature to {}C and get started on step {}.\n\nEstimated completion time for step {} is: {}.\n",
+)
+
+def send_email(message,cc=False,logfile=False,data=None, args=[]):
     """Sends an email to the specified email address. logfile or datafile can
     be attached if desired. used mainly for email updates on progress during
     long measurement cycles. mailer is geophysicslabnotifications@gmail.com.
@@ -27,18 +34,19 @@ def send_email(message,cc=False,logfile=False,datafile=False):
     if not config.EMAIL:
         return
 
+    message = MESSAGES.get(message, message).format(*args)
+    fromaddr = config.EMAIL['from']
+    toaddr = config.EMAIL['to']
     try:
-        toaddr = config.EMAIL
-        fromaddr = "geophysicslabnotifications@gmail.com"
-        pw = 'Laboratory123!'
-
         msg = MIMEMultipart()
         msg['From'] = fromaddr
         msg['To'] = toaddr
+        
         if cc:
             msg['Cc'] = cc
         msg['Subject'] = "Lab Notification"
-        body = 'Hi,\n\n{}\n\nCheers,\nYour Friendly Lab Assistant'.format(message)
+        body = 'Hi,\n\n{}'.format(message)
+        body += '\n\nCheers,\nYour Friendly Lab Assistant' 
         msg.attach(MIMEText(body, 'plain'))
 
         if logfile:
@@ -47,10 +55,10 @@ def send_email(message,cc=False,logfile=False,datafile=False):
             part.add_header('Content-Disposition', 'attachment', filename='controlLog.txt')
             msg.attach(part)
 
-        if datafile:
+        if data is not None:
             part = MIMEBase('application', "octet-stream")
-            part.set_payload(open(datafile, "rb").read())
-            part.add_header('Content-Disposition', 'attachment', filename='labData.txt')
+            part.set_payload(data.to_csv().encode())
+            part.add_header('Content-Disposition', 'attachment', filename='data.csv')
             msg.attach(part)
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -58,17 +66,16 @@ def send_email(message,cc=False,logfile=False,datafile=False):
         server.ehlo()
         server.starttls()
         server.ehlo()
-        server.login(fromaddr, pw)
+        server.login(fromaddr, config.EMAIL['pw'])
         text = msg.as_string()
         if cc:
             server.sendmail(fromaddr, [toaddr,cc], text)
         else:
             server.sendmail(fromaddr, toaddr, text)
         server.quit
-    except Exception as e:
+    except Exception:
         pass
 
-class Messages():
-    device_error = 'I\'ve got some bad news! The {} is no longer sending or receiving messages so I\'m going to shut down the lab until you can come take a look.'
-    delayed_start = 'It just ticked over to {} so I\'m going to set up the instruments and get things underway.'
-    step_complete = "Just letting you know that step {} is now complete! I\'m going to set the temperature to {}C and get started on step {}.\n\nEstimated completion time for step {} is: {}."
+
+
+
