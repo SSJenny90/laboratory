0247e1b409c15e9a697f421b510ea83c020f5824
Samuel Scott Jennings <a1654095@adelaide.edu.au>
2020-08-03 15:03:08 +0930

    move processing out of Laboratory

--- laboratory/laboratory.py
+++ laboratory/laboratory.py
@@ -45,44 +45,11 @@ class Laboratory():
             logger.handlers[0].setLevel('INFO')
             self._debug = False
 
-    def load_data(self, project_folder):
-        """loads a previous experiment for processing and analysis
-
-        :param project_folder: name of experiment
-        :type project_folder: str
-        """
-        data = pd.read_pickle(
-            glob.glob(os.path.join(project_folder, '*.pkl'))[0])
-        self.data = self.process_data(data)
-
-    def process_data(self, data):
-        return processing.process_data(data, 97.686, 2.6)
-        # return processing.process_data(data)
-
-    def restart_from_backup(self):
-        """
-        TODO - reload an aborted experiment and pick up where it left off
-        """
-        # load the pickle file
-        return
-
     def header(self, step, i):
         print('Estimated finish: {}'.format(datetime.strftime(
                 datetime.now() + step.hold_length, '%H:%M %A, %b %d')))
-
         logger.info('Step {}:\n'.format(i+1))
-
-        # print('======================')
-
-        # print('Step {}\n',step[
-        #     ['target_temp','hold_length','heating_rate','buffer']
-        #     ])
-
-
-        # print('======================')
-
         self.display_controlfile(self.controlfile.iloc[[i]])
-        # logger.info(finish_time)
 
     def reconnect(self):
         """Attempts to reconnect to any instruments that have been disconnected"""
@@ -101,7 +68,6 @@ class Laboratory():
     def shut_down(self):
         """Returns the furnace to a safe temperature and closes ports to both the DAQ and LCR. (TODO need to close ports to stage and furnace)
         """
-        # TODO save some information about where to start the next file
         logger.critical("Shutting down the lab...")
         self.furnace.shutdown()
         self.daq.shutdown()
@@ -158,12 +124,8 @@ class Experiment(Laboratory):
         self.sample_area = None
         self.sample_thickness = None
 
-    def run(self, step=None):
-        """Being a new experiment defined by the instructions in controlfile.
-
-        Args:
-            controlfile (str, optional): Path to controlfile containing step by step instructions for the experiment.
-        """
+    def run(self):
+        """Being a new experiment defined by the instructions in controlfile."""
         if not self.project_name:
             raise SetupError('You must specify a project name before beginning an experiment.')
         if not self.control_file:
@@ -194,10 +156,8 @@ class Experiment(Laboratory):
 
         self.shut_down()
 
-
     def thermopower_loop(self, step, i):
-        """ Takes a suite of thermopwoer measurements"""
-        # self.plot2.draw_target_fugacity(step.buffer, [self.controlfile.target_temp.min(), self.controlfile.target_temp.max()])
+        """ Takes a suite of thermopower measurements"""
         
         self.header(step, i)
         self.furnace.timer_duration(hours=4)
@@ -205,10 +165,6 @@ class Experiment(Laboratory):
         target_position = self.stage.find_gradient_position(step.gradient)
         offset = self.stage.home - target_position
         all_steps = np.linspace(target_position,self.stage.home + offset, 20).astype(int)
-        # steps = np.linspace(self.stage.home,target_position,10).astype(int)
-        # more = np.linspace(self.stage.home,opp_target_position,10).astype(int)
-        # all_steps = np.concatenate((steps, more))
-
 
         self.stage.go_to(step)
         time.sleep(60*60)   #wait an hour to thermally equilibrate for the first measurement
@@ -308,7 +264,8 @@ class Experiment(Laboratory):
             :type pressure: str
             """
         self.update_progress_bar('Calculating required gas mix')  
-        log_fugacity, ratio = self.gas.set_to_buffer( buffer=step.buffer,
+        log_fugacity, ratio = self.gas.set_to_buffer( 
+                                buffer=step.buffer,
                                 offset= step.offset,
                                 temp=self.mean_temp, 
                                 gas_type=step.fo2_gas)
@@ -375,9 +332,7 @@ class Experiment(Laboratory):
         self.daq.toggle_switch('thermo')
 
     def move_stage(self,step):
-
       pass
-
 
     def update_progress_bar(self,message=None):
         self.progress_bar.set_postfix_str(message)
@@ -592,12 +547,4 @@ class Experiment(Laboratory):
         print(controlfile.to_string(columns=column_names,
                                     header=column_alias, index=False) + '\n')
 
-    def _print_step(self, step):
-        print('================')
-
-
-
-        print('================')
-
-        pass
 
